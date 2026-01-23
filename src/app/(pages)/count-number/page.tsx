7220d85646982ec531f385b2eaf0fe379a12e1c5
raojiajun <471412450@qq.com>
2026-01-23 15:48:56 +0800

    inti

---
+++ src/app/(pages)/count-number/page.tsx
@@ -0,0 +1,3 @@
+export default function CountNumber() {
+    return <div>fdsafdsa</div>;
+}
--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,17 +1,10 @@
-import type { Metadata, ResolvingMetadata } from 'next';
-import type { FC } from 'react';
+import HomePage from '@/app/(pages)/(home-page)/page';
 
-export const generateMetadata = async (
-    _metadata: Record<string, any>,
-    parent: ResolvingMetadata,
-): Promise<Metadata> => ({
-    title: `首页 | ${(await parent).title?.absolute}`,
-});
-
-const HomePage: FC = () => (
-    <div className="flex flex-col items-center justify-center p-8">
-        <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200">Welcome to Home</h1>
-    </div>
-);
+// export const generateMetadata = async (
+//     _metadata: Record<string, any>,
+//     parent: ResolvingMetadata,
+// ): Promise<Metadata> => ({
+//     title: `首页 | ${(await parent).title?.absolute}`,
+// });
 
 export default HomePage;
